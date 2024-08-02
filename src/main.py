1cb54cbd8f51b740115a12b1b4e439dee84521a3
tw40210 <tw40210@gmail.com>
2024-08-02 14:15:46 +0100

    frontend: If url not found in backend, pass to frontend.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from src.routers import algo_router, backtest_router, data_router, strategy_router
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
 
@@ -39,6 +41,12 @@ app.include_router(
 )
 
 app.mount("/", StaticFiles(directory="./static/build", html=True), name="static")
+
+
+# If url not found in backend, pass to frontend.
+@app.exception_handler(404)
+async def not_found_exception_handler(request, exc):
+    return FileResponse("./static/build/index.html")
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", port=5000, host="0.0.0.0", log_level="info")
